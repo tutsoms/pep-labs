e4794cbdb6121531b0fedc63c29f0268edece03a
tutsoms <tutsoms@gmail.com>
2022-12-22 05:01:33 +0000

    Saved progress on Comparisons 2022-12-22 05:01:33 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -13,9 +13,14 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
-    public boolean isEqual(int a, int b){
+    public boolean isEqual(int a , int b){
+      if (a == b) 
+      return true; 
+      else 
       return false;
+
     }
+    
 
     /**
      * @param a the first number to be compared.
@@ -23,7 +28,10 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if (a > b)
         return true;
+        else
+        return false;
        
     }
     /**
@@ -32,7 +40,10 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return true;
+       if (a < b)
+       return true;
+       else 
+       return false;
     }
     /**
      * @param a the first number to be compared.
@@ -40,7 +51,10 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if (a >= b) 
         return true;
+        else
+        return false;
     }
     /**
      * @param a the first number to be compared.
@@ -48,7 +62,10 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if (a <= b) 
         return true;
+        else
+        return false;
     }
 
     /**
@@ -57,6 +74,9 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if (a != b)
         return true;
+        else
+        return false;
     }
 }
