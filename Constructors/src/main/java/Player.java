fce28e9c43bfd21a7a18614587b46d8149e37c07
tutsoms <tutsoms@gmail.com>
2022-12-29 02:54:48 +0000

    Saved progress on Constructors 2022-12-29 02:54:48 +0000

--- Constructors/src/main/java/Player.java
+++ Constructors/src/main/java/Player.java
@@ -16,7 +16,6 @@
 public class Player {
     public String playerName;
     public int level;
-
     /**
      * this.playerName should be set to playerName.
      *
@@ -31,6 +30,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
+        this.playerName= playerName;
+        this.level = 1;
 
     }
 
@@ -48,6 +49,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level; 
 
     }
 
