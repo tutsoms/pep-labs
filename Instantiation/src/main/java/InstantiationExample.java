c1374140b6a5cc04ae4428846484a9764d5181df
tutsoms <tutsoms@gmail.com>
2022-12-29 02:11:28 +0000

    Saved progress on Instantiation 2022-12-29 02:11:28 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,8 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+     Object alien = new Object();
+        return alien;
     }
 }
