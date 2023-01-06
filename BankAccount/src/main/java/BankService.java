b555dff7d8e6a3ad0ab1e9a1ee67084f0b472538
tutsoms <tutsoms@gmail.com>
2023-01-06 01:35:20 +0000

    Saved progress on BankAccount 2023-01-06 01:35:20 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance += amount;
     }
 
     /**
@@ -32,7 +32,9 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if (this.balance >= amount) {
+            this.balance -=amount;
+        }
     }
 
     /**
@@ -40,6 +42,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
