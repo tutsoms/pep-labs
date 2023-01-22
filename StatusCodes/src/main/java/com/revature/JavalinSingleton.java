0d36508a9f52a4733e35e7b533c238480b71d6f3
tutsoms <tutsoms@gmail.com>
2023-01-22 09:51:01 +0000

    Saved progress on StatusCodes 2023-01-22 09:51:01 +0000

--- StatusCodes/src/main/java/com/revature/JavalinSingleton.java
+++ StatusCodes/src/main/java/com/revature/JavalinSingleton.java
@@ -12,11 +12,9 @@ public class JavalinSingleton {
          * 
          * Note: Please refer to the "StatusCodes.MD" file for more assistance if needed.
          */
-        app.get("/client-side-err", ctx -> {
-        
-            //write logic here
-
-        });
+        app.get("/client-side-err", ctx ->{
+                ctx.status(400);
+            });
 
         return app;
     }
