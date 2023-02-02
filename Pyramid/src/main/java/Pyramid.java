97a7451e32821329bf312be18fccf11ed01cee89
tutsoms <tutsoms@gmail.com>
2023-02-02 23:39:37 +0000

    Saved progress on Pyramid 2023-02-02 23:39:37 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,13 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
-    }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    sb.append("*");
+                }
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
 }
