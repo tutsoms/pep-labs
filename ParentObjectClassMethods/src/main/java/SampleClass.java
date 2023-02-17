38d16862eeb4880507ebad6aa84fb19c52872767
tutsoms <tutsoms@gmail.com>
2023-02-17 01:12:56 +0000

    Saved progress on ParentObjectClassMethods 2023-02-17 01:12:56 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -56,9 +56,19 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other) {
+        if (this.a == other.a && this.b == other.b) {
+            return true;
+        }
+        return false;
+    }
 
 
     //    implement a custom .toString(){} method here.
+    public String toString() {
+        return "SampleClass [a=" + this.a + ", b=" + this.b + "]";
+    }
+}
 
     
-}
+
