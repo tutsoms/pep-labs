7a4ade41d09b36eb4bc3538e27ca343ce6faee34
tutsoms <tutsoms@gmail.com>
2023-02-17 23:48:55 +0000

    Saved progress on LargestSum 2023-02-17 23:48:55 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class LargestSum {
@@ -10,6 +11,14 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        Collections.sort(nums, Collections.reverseOrder());
+        int sum = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (!nums.get(i).equals(nums.get(i+1))) {
+                sum = nums.get(i) + nums.get(i+1);
+                break;
+            }
+        }
+        return sum;
     }
 }
