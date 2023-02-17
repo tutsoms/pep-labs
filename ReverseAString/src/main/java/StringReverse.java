80e28f38347da3627056f4230fc0fa9929874edf
tutsoms <tutsoms@gmail.com>
2023-02-17 23:46:17 +0000

    Saved progress on ReverseAString 2023-02-17 23:46:17 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -13,6 +13,7 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
-    }
+        StringBuilder sb = new StringBuilder(str);
+    return sb.reverse().toString();
+}
 }
